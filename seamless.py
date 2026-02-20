221c6513d8874dbbaff48686a3d618cdd6e9adc8
sebastienchesneau <sebastienchesneau@gmail.com>
2026-02-20 14:09:54 +0100

    add samples

---
+++ seamless.py
@@ -0,0 +1,39 @@
+from transformers import AutoProcessor, SeamlessM4Tv2ForTextToText
+import torch
+import os
+import time
+
+# Chargement du modèle et du processeur
+model_name = "facebook/seamless-m4t-v2-large"
+processor = AutoProcessor.from_pretrained(model_name)
+model = SeamlessM4Tv2ForTextToText.from_pretrained(model_name)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+folder = "./samples"
+
+# Langues (ISO 639-3)
+src_lang = "fra"
+target_langs = ["fra", "eng", "deu", "cmn", "spa"]
+
+for filename in os.listdir(folder):
+    if filename.lower().endswith(".txt"):
+        file_path = os.path.join(folder, filename)
+        print(f"Traduction de : {filename}...")
+
+        with open(file_path, "r", encoding="utf-8") as f:
+          input_text = f.read().strip()
+
+        start = time.time()
+        # Préparation de l'entrée
+        inputs = processor(text=input_text, src_lang=src_lang, return_tensors="pt").to(device)
+
+        # Génération (traduction)
+        for tgt_lang in target_langs:
+            with torch.no_grad():
+                generated_tokens = model.generate(**inputs, tgt_lang=tgt_lang)
+                # Décodage du résultat
+                output_text = processor.batch_decode(generated_tokens, skip_special_tokens=True)[0]
+                print(f"Traduction: {output_text}")
+
+        print(f"✅ Traduit en {str(time.time() - start)} s")
---
+++ whisper.py
@@ -0,0 +1,36 @@
+import whisper
+import os
+import time
+
+# 📁 Dossier contenant les fichiers .mp3
+audio_folder = "./samples2"
+
+# 📦 Charger le modèle Whisper (tu peux changer "base" par "tiny", "small", etc.)
+model = whisper.load_model("large-v3")
+
+def is_segment_to_filter(segment):
+    """Check if segment contains usefull transcription"""
+    return segment["no_speech_prob"] >= 0.8 or segment["temperature"] > 0.2
+
+for filename in os.listdir(audio_folder):
+    if filename.lower().endswith(".mp3"):
+        file_path = os.path.join(audio_folder, filename)
+        print(f"Transcription de : {filename}...")
+
+        # 🧠 Transcription
+        start = time.time()
+        result = model.transcribe(file_path)
+        segments = result["segments"]
+        language = result["language"]
+        transcription = ""
+        for seg in segments:
+            if is_segment_to_filter(seg):
+                continue
+            transcription += " " + seg["text"]
+            
+        if transcription == "":
+            continue
+        print(f"Detected language: {language}")
+        print(f"Transcript: {transcription}")
+        print(f"✅ Transcrit en {str(time.time() - start)} s")
+
